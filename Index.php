9065b9302c77d13fe641948d3176116d509c3ae5
Abhishek kumar <35452609+Abhishek33317@users.noreply.github.com>
2020-02-27 14:18:46 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,12 @@
+<?php
+<html>
+<head>
+</head>
+<body>
+<form action="process.php" method="POST" enctype="multipart/form-data">
+  <input type='file' name='pictures' />
+  <input type='submit' value='Save name' name='upload'>
+</form>
+</body>
+</html>
+?>
