80cb1400bec6c14c608b04bca2cc98aaeac69b87
Abhishek kumar <35452609+Abhishek33317@users.noreply.github.com>
2020-03-12 00:36:23 +0530

    Update Display.php

--- Display.php
+++ Display.php
@@ -4,29 +4,49 @@
  </head>
 <body 
 <?php 
-                     require_once("db_connect.php");
-                     if(isset($_GET['success']))
-                     {
- 
-                     
-                     $viewquery = " select * from images";
-                     $result = mysqli_query($connection,$viewquery);
- 
-                     while($row=mysqli_fetch_assoc($result))
-                     {
-                         $display=$row['image'];
- 
-                        echo '  <div class="card-body">
-                                    <img src="upload/'.$display.'" width="200" heigt="200" class="m-auto">;
-                                </div>';                            
-                     }
-                    }
-                    else
-                    {
-                        header("location: index.php");
-                    }
- 
-                     
-                     ?>
+require_once("db_connect.php");
+if(isset($_GET['success']))
+{
+$viewquery = " select * from student";
+$result = mysqli_query($connection,$viewquery);
+while($row=mysqli_fetch_array($result))
+{
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+  text-align: left;
+}
+}
+</style>
+</head>
+<body>
+<table style="width:100%">
+  <tr>
+    <th>ID</th>
+    <th>Username</th>
+    <th>Image</th>
+</tr>
+<tr>
+    <td><?php echo $row['Id']; ?></td>
+    <td><?php echo $row['username']; ?></td>
+    <td><img src= "<?php echo $row['image'];?>" height="100px" width="100px"></td>
+</tr>
+</table>
+<?php
+}
+}
+else
+{
+header("location: index.php");
+}
+?>
  </body>
 </html>
